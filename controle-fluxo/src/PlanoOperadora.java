6f4f02f134aab730a4b9e64703a2c5290a3ea575
filipe <fstraubel10@gmail.com>
2024-06-11 17:08:06 -0300

    Switch Case sem break

---
+++ controle-fluxo/src/PlanoOperadora.java
@@ -0,0 +1,18 @@
+public class PlanoOperadora {
+    public static void main(String[] args) {
+        String plano = "M";
+
+        switch (plano) {
+            case "T":{
+                System.out.println("5GB youtube");
+            }
+            case "M":{
+                System.out.println("Whats e Instagram Grátis");
+            }
+            case "B":{
+                System.out.println("100 Minutos de ligação");
+            }
+            
+        }
+    }
+}
