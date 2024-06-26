7c99fbc39ba55788b190b07e639afbabb6928fc2
filipe <fstraubel10@gmail.com>
2024-06-26 10:24:15 -0300

    Exemplo Enums

--- exemplo_pacotes/src/Cadastro/SistemaCadastro.java
+++ exemplo_pacotes/src/Cadastro/SistemaCadastro.java
@@ -1,10 +1,17 @@
 package Cadastro;
 
 public class SistemaCadastro {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		//criamos uma pessoa no sistema
+		Pessoa marcos = new Pessoa("123", "Marcos");
+		
+		//definimos o endereço de marcos
+		marcos.setEndereco("RUA DAS MARIAS");
+		
+		//e como definir o nome e cpf do marcos ?
+		
+		//imprimindo o marcos sem o nome e cpf
+		
+		System.out.println(marcos.getNome() + "-" + marcos.getCpf());
 	}
-
 }
---
+++ exemplo_pacotes/src/Estados/SistemaIbge.java
@@ -0,0 +1,19 @@
+package Estados;
+
+public class SistemaIbge {
+
+	public static void main(String[] args) {
+		for(EstadoBrasileiro e: EstadoBrasileiro.values()) {
+			System.out.println(e.getSigla() + " - " + e.getNome() );
+		}
+
+		EstadoBrasileiro eb = EstadoBrasileiro.SANTA_CATARINA;
+				
+		System.out.println(eb.getNome());
+		System.out.println(eb.getSigla());
+		System.out.println(eb.getNomeMaiusculo());
+		System.out.println(eb.getIbge());
+		
+	}
+
+}
