1774c63667845cf224f61f31f5deaa781025e07f
filipe <fstraubel10@gmail.com>
2024-07-02 11:49:07 -0300

    Exemplo separado de abstração

---
+++ exemplo_pacotes/src/MensagensInstantaneas2/ServicoMensagemInstantanea.java
@@ -0,0 +1,7 @@
+package MensagensInstantaneas2;
+
+//a classe MSNMessenger é ou representa
+public abstract class ServicoMensagemInstantanea {
+	public abstract void enviarMensagem();
+	public abstract void receberMensagem();	
+}
